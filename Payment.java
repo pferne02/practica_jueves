7abf1653ce2f54102f8c8b048c2bdd6aabe49599
pferne02 <36732708+pferne02@users.noreply.github.com>
2018-05-08 02:19:23 +0200

    creación clase payment
    Parte de la clase, lo que he podido hacer hoy

---
+++ Payment.java
@@ -0,0 +1,19 @@
+package es.unileon.prg1.buddiesBill2b;
+
+public class Payment{
+	
+	private String from;
+	
+	private String to;
+	
+	Payment(String f, String t){
+		
+		from = f
+		to = t
+		
+	}
+	
+	
+}
+
+	
